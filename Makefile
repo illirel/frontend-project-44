408cae30dd9477e0ec724bdf622fbf8c4c8a6213
illirel <ilya.kononov.09@gmail.com>
2024-03-08 19:54:03 +0500

    6th step

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ brain-even:
 	node bin/brain-even.js
 brain-games:
 	node bin/brain-games.js
-
+brain-calc:
+	node bin/brain-calc.js
 publish:
 	npm publish --dry-run
 lint:
---
+++ bin/brain-calc.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import { greetUser } from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
+
+
+function getRandomOperator() {
+  const operators = ['+', '-', '*'];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  return operators[randomIndex];
+}
+
+
+
+function playCalc() {
+	const name = greetUser();
+	console.log('What is the result of the expression?')
+
+	let correctAnswersCount = 0;
+
+	while (correctAnswersCount < 3) {
+		const operator = getRandomOperator();
+		const randomNumber1 = Math.floor(Math.random() * 100) + 1;
+		const randomNumber2 = Math.floor(Math.random() * 100) + 1;
+		console.log(`Question: ${randomNumber1} ${operator} ${randomNumber2}`);
+		const userAnswer = readlineSync.question('Your answer: ');
+		const correctAnswer = (eval(`${randomNumber1} ${operator} ${randomNumber2}`)).toString();
+
+
+		if (userAnswer !== correctAnswer) {
+			console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+			console.log(`Let's try again, ${name}!`);
+			return;
+		}
+
+		console.log('Correct!');
+		correctAnswersCount += 1;
+	}
+
+  	console.log(`Congratulations, ${name}!`);
+}
+playCalc();
