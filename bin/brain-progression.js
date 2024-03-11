05c6d803071050812416965ff4accd249d13c4de
illirel <ilya.kononov.09@gmail.com>
2024-03-11 17:38:41 +0500

    8th step started

---
+++ bin/brain-progression.js
@@ -0,0 +1,8 @@
+  GNU nano 6.2                           bin/brain-calc.js                                    #!/usr/bin/env node
+import { playProgression } from '../games/brain-progression.js';
+
+playProgression();
+
+
+
+
