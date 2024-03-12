e55032302e42e07977676573b26dad473d038ef9
illirel <ilya.kononov.09@gmail.com>
2024-03-12 13:55:42 +0500

    All Done

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import playPrime from '../games/brain-prime.js';
+
+playPrime();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,8 +1,4 @@
-  GNU nano 6.2                           bin/brain-calc.js                                    #!/usr/bin/env node
-import { playProgression } from '../games/brain-progression.js';
+#!/usr/bin/env node
+import playProgression from '../games/brain-progression.js';
 
 playProgression();
-
-
-
-
